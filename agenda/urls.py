fc868d627d50037d23fab57ef588bce93f20a7d8
jack <jackthebestripper@yahoo.com>
2018-07-20 05:44:27 +0000

    A first functional authentication system

--- agenda/urls.py
+++ agenda/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
+
+from personal_calendar.views import profile
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', login),
+    url(r'^accounts/logout/$', logout, {'next_page':'/accounts/login/'}),
+    url(r'^accounts/profile/$', profile),
 ]
