f72373e58c95ffe0044dcf224f56238f5c5104d7
jack <jackthebestripper@yahoo.com>
2018-07-23 06:33:53 +0000

    account creation

--- usermanagement/urls.py
+++ usermanagement/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
+from .views import create_account
 
 urlpatterns = [
     url(r'^succes/$', TemplateView.as_view(template_name="user/succes.html")),
+    url(r'^create_account/$', create_account),
 ]
--- usermanagement/views.py
+++ usermanagement/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+def create_account(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/user/succes/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'user/create.html', {'form':form})
