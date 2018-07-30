5cdcc4417327926520d2a27d767fc1c0291625fa
jack <jackthebestripper@yahoo.com>
2018-07-30 18:30:40 +0000

    details view completed

--- personal_calendar/urls.py
+++ personal_calendar/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 
-from .views import create, details
+from .views import create, details, delete_participant
 
 urlpatterns = [
     url(r'^create/$', create),
     url(r'^(\d+)/details/$', details),
+    url(r'^(\d+)/participant/(\d+)/delete/$', delete_participant),
 ]
--- personal_calendar/views.py
+++ personal_calendar/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.forms import HiddenInput
 
 from .forms import EventForm, Evenement_ParticipantForm
-from .models import Evenement
+from .models import Evenement, Evenement_Participant
 
 def create(request):
     if request.method == "POST":
@@ -16,13 +18,34 @@ def create(request):
 
 
 def details(request, id):
+    event = Evenement.objects.get(pk=id)
     if request.method == "POST":
         form = Evenement_ParticipantForm(request.POST)
         if form.is_valid():
             form.save()
             return HttpResponseRedirect('/agenda/%s/details/' % id)
     else:
-        form = Evenement_ParticipantForm()
+        form = Evenement_ParticipantForm(initial={'evenement':event})
+        participants = [user.pk for user in event.participants.all()]
+        form.fields['participant'].queryset=User.objects.exclude(
+            pk__in=participants
+        )
+        form.fields['evenement'].widget = HiddenInput()
+
+    return render(request, 'event/details.html', {
+        'event':event,
+        'form':form
+    })
+
+
+def delete_participant(request, id_event, id_participant):
+    if request.method == "POST":
+        evenement = Evenement.objects.get(pk=id_event)
+        participant = User.objects.get(pk=id_participant)
+        a_supprimer = Evenement_Participant.objects.get(
+            evenement=evenement,
+            participant=participant
+        )
         
-    event = Evenement.objects.get(pk=id)
-    return render(request, 'event/details.html', {'event':event, 'form':form})
+        a_supprimer.delete()
+    return HttpResponseRedirect('/agenda/%s/details/' % id_event)
